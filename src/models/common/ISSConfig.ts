ba3de7293d4feb78a33343636ff95a680d94d8ef
taocong <taoconga@isoftstone.com>
2017-02-26 16:55:52 +0800

    updates css

--- src/models/common/ISSConfig.ts
+++ src/models/common/ISSConfig.ts
@@ -88,28 +88,28 @@ export class ISSConfig {
       "image": "http://files.ovdream.com/2016/11/21/583249a931cec.jpg",
       "des":"用户首次注册成为梦想会员后...",
       "price":"¥0.00",
-      "number":"已成交8"
+      "number":"8"
     },
     {
       "title": "媒体推荐",
       "image": "http://files.ovdream.com/2017/01/22/1485050966181.jpg",
       "des":"整合优秀的媒体渠道,为每...",
       "price":"¥9999.00",
-      "number":"已成交8"
+      "number":"8"
     },
     {
       "title": "政策申报",
       "image": "http://files.ovdream.com/2016/11/19/582fe56690ec2.jpg",
-      "des":"依托政策大数据库,针对企业全...",
+      "des":"依托政策大数据库,针对...",
       "price":"¥11399.00",
-      "number":"已成交3"
+      "number":"3"
     },
     {
       "title": "基础服务",
       "image": "http://files.ovdream.com/2016/11/19/582fe1fcc4fe7.jpg",
       "des":"创业起步基础服务",
       "price":"¥6299.00",
-      "number":"已成交3"
+      "number":"3"
     }
 
   ];
@@ -120,28 +120,28 @@ export class ISSConfig {
       "image": "http://files.ovdream.com/2016/11/19/582fe1fcc4fe7.jpg",
       "des":"创业起步基础服务",
       "price":"¥6299.00",
-      "number":"已成交3"
+      "number":"3"
     },
     {
       "title": "梦想会员",
       "image": "http://files.ovdream.com/2016/11/21/583249a931cec.jpg",
       "des":"用户首次注册成为梦想会员后...",
       "price":"¥0.00",
-      "number":"已成交8"
+      "number":"8"
     },
     {
       "title": "媒体推荐",
       "image": "http://files.ovdream.com/2017/01/22/1485050966181.jpg",
       "des":"整合优秀的媒体渠道,为每...",
       "price":"¥9999.00",
-      "number":"已成交8"
+      "number":"8"
     },
     {
       "title": "政策申报",
       "image": "http://files.ovdream.com/2016/11/19/582fe56690ec2.jpg",
-      "des":"依托政策大数据库,针对企业全...",
+      "des":"依托政策大数据库,针对...",
       "price":"¥11399.00",
-      "number":"已成交3"
+      "number":"3"
     }
 
   ];
@@ -152,28 +152,28 @@ export class ISSConfig {
       "image": "http://files.ovdream.com/2017/01/22/1485050966181.jpg",
       "des":"整合优秀的媒体渠道,为每...",
       "price":"¥9999.00",
-      "number":"已成交8"
+      "number":"8"
     },
     {
       "title": "政策申报",
       "image": "http://files.ovdream.com/2016/11/19/582fe56690ec2.jpg",
-      "des":"依托政策大数据库,针对企业全...",
+      "des":"依托政策大数据库,针对...",
       "price":"¥11399.00",
-      "number":"已成交3"
+      "number":"3"
     },
     {
       "title": "基础服务",
       "image": "http://files.ovdream.com/2016/11/19/582fe1fcc4fe7.jpg",
       "des":"创业起步基础服务",
       "price":"¥6299.00",
-      "number":"已成交3"
+      "number":"3"
     },
     {
       "title": "梦想会员",
       "image": "http://files.ovdream.com/2016/11/21/583249a931cec.jpg",
       "des":"用户首次注册成为梦想会员后...",
       "price":"¥0.00",
-      "number":"已成交8"
+      "number":"8"
     },
 
   ];
@@ -185,28 +185,28 @@ export class ISSConfig {
       "image": "http://files.ovdream.com/2016/11/21/583249a931cec.jpg",
       "des":"用户首次注册成为梦想会员后...",
       "price":"¥0.00",
-      "number":"已成交8"
+      "number":"8"
     },
     {
       "title": "媒体推荐",
       "image": "http://files.ovdream.com/2017/01/22/1485050966181.jpg",
       "des":"整合优秀的媒体渠道,为每...",
       "price":"¥9999.00",
-      "number":"已成交8"
+      "number":"8"
     },
     {
       "title": "政策申报",
       "image": "http://files.ovdream.com/2016/11/19/582fe56690ec2.jpg",
-      "des":"依托政策大数据库,针对企业全...",
+      "des":"依托政策大数据库,针对...",
       "price":"¥11399.00",
-      "number":"已成交3"
+      "number":"3"
     },
     {
       "title": "基础服务",
       "image": "http://files.ovdream.com/2016/11/19/582fe1fcc4fe7.jpg",
       "des":"创业起步基础服务",
       "price":"¥6299.00",
-      "number":"已成交3"
+      "number":"3"
     }
 
   ];
